2a2386d18a887645deda0189c6dca931e46c12a8
Ítalo Vitorino <italo.vitorinobs@hotmail.com>
2025-09-18 19:20:11 -0300

    Inicializando back

---
+++ codigo/back/src/main/java/com/lds/aluguel_carros/AluguelCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.lds.aluguel_carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelCarrosApplication.class, args);
+	}
+
+}
